69281b500119d1b2706272bc4ab1c0cbb5da982c
chime3 <chidev0104@gmail.com>
2023-01-08 17:48:58 +0800

    changing google signin functionality

--- client/src/pages/login/pages/SignIn.jsx
+++ client/src/pages/login/pages/SignIn.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { useGoogleLogin } from '@react-oauth/google'
 import axios from 'axios'
 import { encryption, setCurrency, uploadFile } from '../functions/main_functions'
 import { googleInstance } from '../../../utils/google'
 import { handleAuthClick } from '../../../utils/googlev2'
+import jwt_decode from 'jwt-decode'
 
 const Container = styled.div`
     width: 100%;
@@ -53,9 +54,81 @@ const Button = styled.button`
 var access_token;
 var email
 
+const CLIENT_ID = '481242808170-q83c0q6apdgi7oedo1gv9dv1fmn3m09j.apps.googleusercontent.com'
+const SCOPES = 'https://www.googleapis.com/auth/drive.file'
+
 const SignIn = () => {
 
-    // const login = useGoogleLogin({
+    const [user, setUser] = useState({})
+    const [tokenClient, setTokenClient] = useState({})
+    
+    function handleCallBackResponse(response){
+        console.log("Encoded JWT ID token: " + response.credential)
+        var userObject = jwt_decode(response.credential)
+        console.log(userObject)
+        setUser(userObject)
+        console.log(userObject.email)
+        email = userObject.email
+        console.log("Email: ", email)
+    }
+
+    function authorize() {
+        tokenClient.requestAccessToken()
+    }
+
+    useEffect(() => {
+        /* global google */
+        const google = window.google
+        google.accounts.id.initialize({
+            client_id: CLIENT_ID,
+            callback: handleCallBackResponse 
+        })
+        google.accounts.id.renderButton(
+            document.getElementById("signInDiv"),
+            {theme: 'outline', size: 'large'}
+        )
+        //Access tokens to upload to user drive
+        //tokenClient 
+        setTokenClient(google.accounts.oauth2.initTokenClient({
+            client_id: CLIENT_ID,
+            scope: SCOPES,
+            callback: (tokenResponse) => {
+                console.log(tokenResponse)
+                //we now have access to live token to use for any google API
+            }
+        }))
+        // google.accounts.id.prompt()
+    }, [])
+    
+
+  return (
+    <Container>
+        <Wrapper>
+        <Title>
+            Me3
+        </Title>
+        <Google>
+        Google
+        </Google>
+        <Text>
+            Text
+        </Text>
+        <Disclaimer>
+            By continuing, you agree to our Terms of Use and Privacy Policy
+        </Disclaimer>
+        <Button id = 'signInDiv'></Button>
+        {user && 
+            <Button onClick={authorize}>Authorize</Button>
+        }
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default SignIn
+
+
+// const login = useGoogleLogin({
     //     onSuccess:  async function response(tokenResponse) {
     //         try {
     //             const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo",{
@@ -74,28 +147,3 @@ const SignIn = () => {
     //         }
     //     }
     //   })
-
-  return (
-    <Container>
-        <Wrapper>
-        <Title>
-            Me3
-        </Title>
-        <Google>
-        Google
-        </Google>
-        <Text>
-            Text
-        </Text>
-        <Disclaimer>
-            By continuing, you agree to our Terms of Use and Privacy Policy
-        </Disclaimer>
-        <Button onClick={() => handleAuthClick()}>
-            Sign in with Google
-        </Button>
-        </Wrapper>
-    </Container>
-  )
-}
-
-export default SignIn
--- client/src/utils/googlev2.js
+++ client/src/utils/googlev2.js
@@ -1,93 +1,93 @@
-// import gapi from 'gapi'
-//Set credentials from google cloud console 
-const CLIENT_ID = ''
-const API_KEY = ''
+// // import gapi from 'gapi'
+// //Set credentials from google cloud console 
+// const CLIENT_ID = '481242808170-q83c0q6apdgi7oedo1gv9dv1fmn3m09j.apps.googleusercontent.com'
+// const API_KEY = ''
 
-//Discovery URL for APIs used by quickstart 
-const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
+// //Discovery URL for APIs used by quickstart 
+// const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
 
-//Set API access scope before proceeding with authorization request 
-const SCOPES = 'https://www.googleapis.com/auth/drive.file'
-let tokenClient 
-let gapiInited = false
-let gisInited = false
+// //Set API access scope before proceeding with authorization request 
+// const SCOPES = 'https://www.googleapis.com/auth/drive.file'
+// let tokenClient 
+// let gapiInited = false
+// let gisInited = false
 
-//Callback after api.js is loaded
-export function gapiLoaded() {
-    gapiInited.load('client', initGapiClient)
-}
+// //Callback after api.js is loaded
+// export function gapiLoaded() {
+//     gapiInited.load('client', initGapiClient)
+// }
 
-//Callback after API client is loaded. 
-async function initGapiClient() {
-    await gapiInited.client.init({
-        apiKey: API_KEY,
-        discoveryDocs: [DISCOVERY_DOC]
-    })
-    gapiInited = true
-    maybeEnableButtons()
-}
+// //Callback after API client is loaded. 
+// async function initGapiClient() {
+//     await gapiInited.client.init({
+//         apiKey: API_KEY,
+//         discoveryDocs: [DISCOVERY_DOC]
+//     })
+//     gapiInited = true
+//     maybeEnableButtons()
+// }
 
-//Callback after Google Identity Services are loaded
-export function gisLoaded() {
-    /* global google */
-    tokenClient = google.accounts.oauth2.initTokenClient({
-        client_id: CLIENT_ID,
-        scope: SCOPES,
-        callback: '', //defined later
-    })
-    gisInited = true
-    maybeEnableButtons()
-}
+// //Callback after Google Identity Services are loaded
+// export function gisLoaded() {
+//     /* global google */
+//     tokenClient = google.accounts.oauth2.initTokenClient({
+//         client_id: CLIENT_ID,
+//         scope: SCOPES,
+//         callback: '', //defined later
+//     })
+//     gisInited = true
+//     maybeEnableButtons()
+// }
 
-//Enables user interaction after all libraries are loaded
-function maybeEnableButtons(){
-    if (gapiInited && gisInited) {
-        document.getElementById('authorize_button').style.visibility = 'visible'
-    }
-}
+// //Enables user interaction after all libraries are loaded
+// function maybeEnableButtons(){
+//     if (gapiInited && gisInited) {
+//         document.getElementById('authorize_button').style.visibility = 'visible'
+//     }
+// }
 
-//Sign in the user upon button click 
-export function handleAuthClick() {
-    tokenClient.callback = async(resp) =>{
-        if (resp.error !== undefined) {
-            throw(resp)
-        }
-        document.getElementById('authorize_button').value = 'Refresh'
-        await uploadFile()
-    }
+// //Sign in the user upon button click 
+// export function handleAuthClick() {
+//     tokenClient.callback = async(resp) =>{
+//         if (resp.error !== undefined) {
+//             throw(resp)
+//         }
+//         document.getElementById('authorize_button').value = 'Refresh'
+//         await uploadFile()
+//     }
 
-    if (gapiInited.client.getToken() === null) {
-        //Prompt the user to select a Google Account and ask for consent to share their data 
-        //when establishing a new session
-        tokenClient.requestAccessToken({prompt: 'consent'})
-    } else {
-        //skip display of account chooser and consent dialog for an existing session
-        tokenClient.requestAccessToken({prompt: ''})
-    }
-}
+//     if (gapiInited.client.getToken() === null) {
+//         //Prompt the user to select a Google Account and ask for consent to share their data 
+//         //when establishing a new session
+//         tokenClient.requestAccessToken({prompt: 'consent'})
+//     } else {
+//         //skip display of account chooser and consent dialog for an existing session
+//         tokenClient.requestAccessToken({prompt: ''})
+//     }
+// }
 
-//Upload file to Google Drive 
-async function uploadFile() {
-    var fileContent = 'Hello world'
-    var file = new Blob([fileContent], {type: 'text/plain'})
-    var metdata = {
-        'name' : 'sample-file-via-js', // Filename at Google Drive 
-        'mimeType': 'text/plain'
-    }
-    /* global gapi */
-    var accessToken = gapi.auth.getToken().access_token
-    var form = new FormData()
-    form.append('metadata', new Blob([JSON.stringify(metdata)], {type:'application/json'}))
-    form.append('file', file)
+// //Upload file to Google Drive 
+// async function uploadFile() {
+//     var fileContent = 'Hello world'
+//     var file = new Blob([fileContent], {type: 'text/plain'})
+//     var metdata = {
+//         'name' : 'sample-file-via-js', // Filename at Google Drive 
+//         'mimeType': 'text/plain'
+//     }
+//     /* global gapi */
+//     var accessToken = gapi.auth.getToken().access_token
+//     var form = new FormData()
+//     form.append('metadata', new Blob([JSON.stringify(metdata)], {type:'application/json'}))
+//     form.append('file', file)
 
-    var xhr = new XMLHttpRequest()
-    xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id')
-    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken)
-    xhr.responseType = 'json'
-    xhr.onload = () => {
-        console.log('File uploaded successfully. The Google file id is <b>' + xhr.response.id + '</b>')
-    }
-}
+//     var xhr = new XMLHttpRequest()
+//     xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id')
+//     xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken)
+//     xhr.responseType = 'json'
+//     xhr.onload = () => {
+//         console.log('File uploaded successfully. The Google file id is <b>' + xhr.response.id + '</b>')
+//     }
+// }
 
 
 
