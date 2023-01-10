a870f847ce6088b2facb22e1d31666e7aa9b0b71
chime3 <chidev0104@gmail.com>
2023-01-10 16:09:52 +0800

    google sso + upload file to gDrive success

--- client/src/pages/login/functions/main_functions.js
+++ client/src/pages/login/functions/main_functions.js
@@ -1,4 +1,3 @@
-import { retrieveAuthData, saveAuthData } from "../../../api/config/storage.js"
 import storageConst from "../../../api/config/storage.js"
 import { keyExchange, registerUser, setFileId, uploadFileGdrive } from "../../../api/login/loginApis.js"
 import { Encrypt, getRandomString } from "../../../utils/EncryptDecrpyt.js"
@@ -17,26 +16,12 @@ import {ChaCha20Poly1305, NONCE_LENGTH} from '@stablelib/chacha20poly1305'
 
 async function encryption(email, accessToken) {
     
-    const rsaKeyPair =  
-        pki.rsa.generateKeyPair({
-        bits: 1024,
-        e: 0x10001,
-    })
-    
+    const rsaKeyPair = genKeyPair()
     const {privateKey, publicKey} = rsaKeyPair
-
-    const priKey = util.encode64(
-        asn1.toDer(pki.privateKeyToAsn1(privateKey)).getBytes(),
-    )
-    
-    const pubKey = util.encode64(
-    asn1.toDer(pki.publicKeyToAsn1(publicKey)).getBytes(),
-    )
-
+    const priKey = encodedPriKey(privateKey)
+    const pubKey = encodedPubKey(publicKey)
     localStorage.setItem(storageConst.CLIENT_PUBLICKEY, pubKey)
     localStorage.setItem(storageConst.CLIENT_PRIVATEKEY, priKey)
-
-
     try {
         const data = await keyExchange(email, pubKey);
 
@@ -44,34 +29,61 @@ async function encryption(email, accessToken) {
             return null;
         }
         localStorage.setItem(storageConst.SERVER_PUBLICKEY, data);
-
         const registerRes = await registerUser(email);
-        console.log('registerData.data', registerRes.data, typeof registerRes.data)
-        const chachaKey = stableHex.decode(
-            util.bytesToHex(
-              privateKey.decrypt(util.decode64(registerRes.data.secret), 'RSA-OAEP'),
-            ),
-        );
-        console.log('chachaKey', chachaKey);
-        const decipher = new ChaCha20Poly1305(chachaKey)
-        const array = stable64.decode(registerRes.data.data)
-        const decrypted = stableUtf8.decode(
-            decipher.open(array.slice(0, NONCE_LENGTH), array.slice(NONCE_LENGTH)),
-        )
+        const chachaKey = getChaChaKey(privateKey, registerRes.data.secret)
+        const decrypted = decryptChaChaKey(chachaKey, registerRes.data.data)
         console.info('decrypted', JSON.parse(decrypted))
-        console.log('object.keys(decrypted)', Object.keys(JSON.parse(decrypted)));
         const isExistingUser = Object.keys(JSON.parse(decrypted)).length === 1
-        console.log('isExistingUser', isExistingUser);
         if (!isExistingUser) {
             const fileToUploadToDrive = await extractNewUserData(decrypted)
             console.log(fileToUploadToDrive)
             uploadFileToGDrive(accessToken, fileToUploadToDrive)        
         } else {
-            console.log("existing user function does not work")    
+            console.log("existing user function is working")    
         } 
     } catch (error) {
         console.error(error);
     };    
+}
+
+function genKeyPair(){
+    const rsaKeyPair = pki.rsa.generateKeyPair({
+        bits: 1024,
+        e: 0x10001,
+    })
+    return rsaKeyPair
+}
+
+function encodedPriKey(privateKey){
+    const priKey = util.encode64(
+        asn1.toDer(pki.privateKeyToAsn1(privateKey)).getBytes(),
+    )
+    return priKey
+}
+
+function encodedPubKey(publicKey){
+    const pubKey = util.encode64(
+        asn1.toDer(pki.publicKeyToAsn1(publicKey)).getBytes(),
+        )
+    return pubKey
+}
+
+function getChaChaKey(privateKey, secret){
+    const chachaKey = stableHex.decode(
+        util.bytesToHex(
+          (privateKey).decrypt(util.decode64(secret), 'RSA-OAEP'),
+        ),
+    );
+    return chachaKey
+}
+
+function decryptChaChaKey(chaChaKey, registerResData) {
+    const decipher = new ChaCha20Poly1305(chaChaKey)
+    const array = stable64.decode(registerResData)
+    const decrypted = stableUtf8.decode(
+        decipher.open(array.slice(0, NONCE_LENGTH), array.slice(NONCE_LENGTH)),
+    )
+    return decrypted
 }
 
 async function extractNewUserData(decrypted){
@@ -98,14 +110,12 @@ async function extractNewUserData(decrypted){
 }
 
  const uploadFileToGDrive = async (accessToken, file) => {
-    // var fileContent = file
     console.log(file)
     const uploadFile = new Blob([JSON.stringify(file)], {type: 'text/plain'})
     const metdata = {
         'name' : 'ME3_KEY_DEV.json', // Filename at Google Drive 
         'mimeType': 'text/plain'
     }
-    // var accessToken = accessToken
     var form = new FormData()
     form.append('metadata', new Blob([JSON.stringify(metdata)], {type:'application/json'}))
     form.append('file', uploadFile)
@@ -141,125 +151,3 @@ async function setCurrency() {
 }
 
 export {setCurrency, encryption}
-
-
-// await keyExchange(email, pubKey)
-    //     .then(data => {
-    //         localStorage.setItem(storageConst.SERVER_PUBLICKEY, data)
-    //         registerUser(email)
-    //             .then(registerData => {
-    //                 console.log(registerData.data.secret, typeof registerData.data.secret)
-    //                 const chachaKey = stableHex.decode(
-    //                     util.bytesToHex(
-    //                       privateKey.decrypt(util.decode64(registerData.data.secret), 'RSA-OAEP'),
-    //                     ),
-    //                 )
-    //                 console.log(chachaKey)
-    //                 const decipher = new ChaCha20Poly1305(chachaKey)
-    //                 const array = stable64.decode(registerData.data.data)
-    //                 const decrypted = stableUtf8.decode(
-    //                     decipher.open(array.slice(0, NONCE_LENGTH), array.slice(NONCE_LENGTH)),
-    //                   )
-    //                 console.log(decrypted)
-    //                 // const check = Object.keys(decrypted).length
-    //                 //check for existing user 
-    //                 const isExistingUser = Object.keys(decrypted).length === 1
-    //                 if(!isExistingUser) {
-    //                     const fileToUploadToDrive = await extractNewUserData(decrypted)
-    //                     console.log(fileToUploadToDrive)
-    //                     uploadFileToGDrive(accessToken, fileToUploadToDrive)
-    //                 }else{
-    //                     console.log("existing user function does not work")
-    //                 }      
-    //             })
-    //     })
-
-
-// if (check !== 1) {
-                    //     const obj = JSON.parse(decrypted)
-                    //     var token = obj.token
-                    //     console.log(token)
-                    //     var password = obj.password
-                    //     var salt = obj.salt
-                    //     var uid = obj.uid
-
-                    //     localStorage.setItem(storageConst.LIGHT_TOKEN, token)
-                    //     localStorage.setItem(storageConst.CLIENT_SALT, salt)
-
-                    //     Encrypt(getRandomString(40) + new Date().getTime(), password)
-                    //     .then(data => {
-                    //         const fileObject = {
-                    //             uid: uid,
-                    //             password: password,
-                    //             salt: salt,
-                    //             key: data
-                    //         }
-                    //         uploadFileToDrive(url,token,fileObject)
-                    //     })
-                    //     .catch(error => {
-                    //         logger('error', error)
-                    //     })   
-                    // }else{
-                    //     var token = decrypted.token
-                    //     if (token != undefined){
-                    //         localStorage.setItem(storageConst.LIGHT_TOKEN, token)
-                    //     }
-                    // }
-                    // if (key != undefined) {
-                    //     var token = registerData.data.token
-                    //     // var output = setDecrypt(privateKey, registerData.data.key)
-                    //     // let responseData = JSON.parse(output)
-                    //     localStorage.setItem(storageConst.LIGHT_TOKEN, token)
-                    //     localStorage.setItem(storageConst.CLIENT_SALT, responseData.salt)
-
-                    //     var password = responseData.password
-                    //     var salt = responseData.salt
-                    //     var uid = responseData.uid
-
-                        // Encrypt(getRandomString(40) + new Date().getTime(), password)
-                        //     .then(data => {
-                        //         const fileObject = {
-                        //             uid: uid,
-                        //             password: password,
-                        //             salt: salt,
-                        //             key: data
-                        //         }
-                        //         uploadFileToDrive(url,fileObject,token)
-                        //     })
-                        //     .catch(error => {
-                        //         logger('error', error)
-                        //     })
-                    // }else {
-                    //     var token = registerData.data.data
-                    //     console.log(token)
-                    //     if (token != undefined) {
-                    //         localStorage.setItem(storageConst.LIGHT_TOKEN, token)
-                    //         // setFileId()
-                    //         // getAllWallets()
-                    //     }
-                    // }
-
-
-// async function uploadFile(file) {
-//     var accessToken = await retrieveAuthData(storageConst.ACCESS_TOKEN)
-//     let fileName = 'ME3_KEY.json'
-//     if (apiConst.BASE_URL === 'https://avarta-official-dev.avarta.io/me3-api'){
-//         fileName = 'ME3_KEY_DEV.json'
-//     }else if (apiConst.BASE_URL === 'https://pre-wallet-prod.me3.io/me3-api'){
-//         fileName = 'ME3_KEY_PRE_PROD.json'
-//     }else{
-//         fileName = 'ME3_KEY.json' 
-//     }
-    // return new Promise(async function(resolve,reject){
-    //     gdrive.files
-    //         .newMultipartUploader()
-    //         .setData(file, MimeTypes.json)
-    //         .setRequestBody({
-    //             name: fileName
-    //         })
-    //         .execute()
-    //         .then(data =>{
-    //             saveAuthData(storageConst.FILE_ID, data.id)
-    //         })
-    // })
-// }
